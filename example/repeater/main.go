eab21cd2b7b50c8f7c2a245e44742c0755b35fbd
ligen131 <i@ligen131.com>
2023-11-16 19:47:25 +0800

    :sparkles: feat: 添加复读机插件示例

---
+++ example/repeater/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+var logs *zap.Logger
+
+const path = "config.yml" // 配置文件路径，默认是当前文件夹下的 config.yml
+
+type ServerConfig struct {
+	Hostname string `config:"hostname"`
+	Port     int    `config:"port"`
+}
+type DatabaseConfig struct {
+	Hostname string `config:"hostname"`
+	Port     int    `config:"port"`
+	User     string `config:"user"`
+	Password string `config:"password"`
+	DbName   string `config:"dbName"`
+	SslMode  bool   `config:"sslMode"`
+	TimeZone string `config:"timeZone"`
+}
+type YamlConfiguration struct {
+	Server               ServerConfig   `config:"server"`
+	Database             DatabaseConfig `config:"database"`
+	PluginCenterEndpoint string         `config:"plugin-center-endpoint"`
+	PluginEndpoint       string         `config:"plugin-endpoint"`
+}
+
+// 读取配置文件
+func YamlConfigLoad(path string) (YamlConfiguration, error) {
+	// 设置选项支持 ENV 解析
+	config.WithOptions(config.ParseEnv)
+
+	// 添加驱动程序以支持 yaml 内容解析
+	config.AddDriver(yaml.Driver)
+	config.WithOptions(func(opt *config.Options) {
+		// 设置 struct tag 名称
+		opt.DecoderConfig.TagName = "config"
+	})
+
+	// 文件读取
+	configuration := YamlConfiguration{}
+	err := config.LoadFiles(path)
+	if err != nil {
+		logs.Error("Read config file from "+path+"failed. ", zap.Error(err))
+		return configuration, err
+	}
+
+	// 解析 YAML
+	err = config.Decode(&configuration)
+	if err != nil {
+		logs.Error("Decode config file from "+path+"failed. ", zap.Error(err))
+		return configuration, err
+	}
+	logs.Info("Read config file from "+path, zap.Any("configuration", configuration))
+
+	return configuration, nil
+}
+
+// 将请求绑定到结构体
+func Bind(c echo.Context, obj interface{}) (bool, error) {
+	err := c.Bind(&obj)
+	if err != nil {
+		logs.Warn("Failed to parse request data.", zap.Error(err))
+		return false, c.JSON(http.StatusBadRequest, "Failed to parse request data.")
+	}
+	logs.Debug("Parsed struct:", zap.Any("obj", obj))
+	return true, nil
+}
+
+// Plugin Center 请求结构体
+type MessageInfo struct {
+	MessageID string      `json:"message_id"`
+	Agent     string      `json:"agent"`
+	GroupID   string      `json:"group_id"`
+	GroupName string      `json:"group_name"`
+	UserID    string      `json:"user_id"`
+	UserName  string      `json:"user_name"`
+	Time      int64       `json:"time"`
+	Message   string      `json:"message"`
+	IsMention bool        `json:"is_mention"`
+	Param     interface{} `json:"param"`
+}
+
+type MessageResponse struct {
+	IsReply bool     `json:"is_reply"`
+	Message []string `json:"message"`
+}
+
+var messageRecord = make(map[string][]MessageInfo)
+
+const repeatCount = 3
+
+func process(c echo.Context) error {
+	logs.Debug("POST /")
+
+	// 将请求绑定到结构体
+	message := MessageInfo{}
+	_ok, err := Bind(c, &message)
+	if !_ok {
+		// 失败返回 400 Bad Request
+		return err
+	}
+
+	/*************** 插件处理逻辑 ***************/
+	logs.Debug("Message:", zap.Any("message", message))
+	if message.GroupID == "" {
+		return c.JSON(http.StatusOK, MessageResponse{
+			IsReply: false,
+			Message: []string{},
+		})
+	}
+
+	messageRecord[message.GroupID] = append(messageRecord[message.GroupID], message)
+	if l := len(messageRecord[message.GroupID]); l >= repeatCount {
+		if messageRecord[message.GroupID][l-1].Message == messageRecord[message.GroupID][l-2].Message &&
+			messageRecord[message.GroupID][l-2].Message == messageRecord[message.GroupID][l-3].Message {
+			reply := []string{messageRecord[message.GroupID][l-1].Message}
+			messageRecord[message.GroupID] = []MessageInfo{}
+			return c.JSON(http.StatusOK, MessageResponse{
+				IsReply: true,
+				Message: reply,
+			})
+		}
+	}
+	return c.JSON(http.StatusOK, MessageResponse{
+		IsReply: false,
+		Message: []string{},
+	})
+}
+
+type PluginParam struct {
+	Key         string `json:"key"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+}
+type PluginInfo struct {
+	ID          string        `json:"id"          `
+	Name        string        `json:"name"        `
+	Author      string        `json:"author"      `
+	Description string        `json:"description" `
+	Prompt      string        `json:"prompt"      `
+	Params      []PluginParam `json:"param"       `
+	Format      []string      `json:"format"      `
+	Example     []string      `json:"example"     `
+	Url         string        `json:"url"         `
+}
+
+func Register(pluginCenterEndpoint string, pluginEndpoint string) error {
+	/****************** 注册插件 ******************/
+	jsonStr, _ := json.Marshal(PluginInfo{
+		ID:          "repeater",
+		Name:        "复读机",
+		Author:      "ligen131",
+		Description: "复读机，需要接收所有消息",
+		Prompt:      "复读机",
+		Params:      []PluginParam{},
+		Format:      []string{},
+		Example: []string{
+			"调用",
+		},
+		Url: pluginEndpoint,
+	})
+	req, _ := http.NewRequest("POST", pluginCenterEndpoint+"/plugin/register", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		if resp == nil {
+			logs.Error("POST Plugin Center Register Endpoint failed", zap.Error(err))
+		} else {
+			logs.Error("POST Plugin Center Register Endpoint failed", zap.Int("StatusCode", resp.StatusCode), zap.Error(err))
+		}
+		return err
+	}
+	return nil
+}
+
+func main() {
+	// logger 初始化
+	logs, _ = zap.NewDevelopment()
+
+	// 读取配置文件
+	config, err := YamlConfigLoad(path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = Register(config.PluginCenterEndpoint, config.PluginEndpoint)
+	if err != nil {
+		logs.Error("Register failed", zap.Error(err))
+	}
+	// 每隔 1 分钟自动注册一次插件
+	ticker := time.NewTicker(time.Minute)
+	go func() {
+		for range ticker.C {
+			err := Register(config.PluginCenterEndpoint, config.PluginEndpoint)
+			if err != nil {
+				logs.Error("Register failed", zap.Error(err))
+			}
+		}
+	}()
+
+	// 配置后端服务
+	e := echo.New()
+	e.POST("/", process)
+
+	// 启动！
+	e.Logger.Fatal(e.Start(config.Server.Hostname + ":" + fmt.Sprint(config.Server.Port)))
+}
